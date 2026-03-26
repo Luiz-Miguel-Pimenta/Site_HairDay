02313ce2d67f262daed118ec7c97d0557210befb
Luiz-Miguel-Pimenta <Miguel876528@gmail.com>
2026-03-26 15:31:13 -0300

    coloquei o webpack server e o plugin de html webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,10 @@
 
 // O Webpack pega todos os arquivos, entende como eles se conectam (quem importa quem) e
 // gera um único arquivo (ou poucos) otimizado e pronto para o navegador ler.
-
 const path = require("path");
+
+const HtmlwebpackPlugin = require("html-webpack-plugin"); //Importa o plugin HTML-webpack
+
 /*
   Essa linha importa um módulo nativo do Node.js chamado path. Ele serve 
   para lidar com caminhos de pastas e arquivos de forma que funcione tanto 
@@ -11,28 +13,51 @@ const path = require("path");
 */
 
 module.exports = {
-    target: "web", //Informa ao Webpack que o código que ele vai gerar é para rodar em um navegador (web).
+  target: "web", //Informa ao Webpack que o código que ele vai gerar é para rodar em um navegador (web).
 
-    mode: "development", 
-    /*
-      Webpack não vai "minificar" o código (deixar tudo em uma linha só), facilitando a leitura 
-      e a depuração (debug) enquanto desenvolve.
-    */
+  mode: "development", 
+  /*
+    Webpack não vai "minificar" o código (deixar tudo em uma linha só), facilitando a leitura 
+    e a depuração (debug) enquanto desenvolve.
+  */
 
-    entry: path.resolve(__dirname, "src", "main.js"),
-    /*
-      Aqui diz: "Webpack, comece a ler o projeto a partir deste arquivo".
-      O "path.resolve" garante que ele encontre a pasta "src" e o arquivo "main.js" 
-      a partir da pasta atual do projeto (__dirname).
-     */
+  entry: path.resolve(__dirname, "src", "main.js"),
+  /*
+    Aqui diz: "Webpack, comece a ler o projeto a partir deste arquivo".
+    O "path.resolve" garante que ele encontre a pasta "src" e o arquivo "main.js" 
+    a partir da pasta atual do projeto (__dirname).
+  */
 
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist")
-    },
-    /* 
-      Aqui define o que o Webpack deve "cuspir" no final:
-        "filename": O nome do arquivo final empacotado (main.js).
-        "path": A pasta onde esse arquivo será salvo. No caso, ele criará (ou usará) uma pasta chamada dist (de distribution).
-    */
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  /* 
+    Aqui define o que o Webpack deve "cuspir" no final:
+      "filename": O nome do arquivo final empacotado (main.js).
+      "path": A pasta onde esse arquivo será salvo. No caso, ele criará (ou usará) uma pasta chamada dist (de distribution).
+  */
+
+  //webpack-dev-server cria um servidor local no computador para rodar o site enquanto o constrói. 
+  devServer: {
+
+  //Diz ao servidor onde estão os arquivos que ele deve "servir" ao navegador.
+  static: {
+    directory: path.join(__dirname, "dist"),
+  },
+    port: 3000,
+    open: true,
+    liveReload: true,
+  },
+
+
+  /*
+    O Webpack usa o campo plugins para adicionar funcionalidades extras que vão além de apenas "ler arquivos". 
+    Pense nos plugins como superpoderes que dá ao Webpack para realizar tarefas complexas de automação. 
+  */
+  plugins: [
+    new HtmlwebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+    }),
+  ],
 }
