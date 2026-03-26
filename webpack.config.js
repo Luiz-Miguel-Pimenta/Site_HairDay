6ca6dfc3bcaec44f0dc4f356c09a738774be4a2f
Luiz-Miguel-Pimenta <Miguel876528@gmail.com>
2026-03-26 14:53:20 -0300

    Adicionei o webpack ao projeto

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+// O Webpack é um "empacotador" (bundler) de módulos para aplicações JavaScript modernas.
+
+// O Webpack pega todos os arquivos, entende como eles se conectam (quem importa quem) e
+// gera um único arquivo (ou poucos) otimizado e pronto para o navegador ler.
+
+const path = require("path");
+/*
+  Essa linha importa um módulo nativo do Node.js chamado path. Ele serve 
+  para lidar com caminhos de pastas e arquivos de forma que funcione tanto 
+  no Windows quanto no Linux/Mac, evitando erros de barras invertidas (\) ou normais (/).
+*/
+
+module.exports = {
+    target: "web", //Informa ao Webpack que o código que ele vai gerar é para rodar em um navegador (web).
+
+    mode: "development", 
+    /*
+      Webpack não vai "minificar" o código (deixar tudo em uma linha só), facilitando a leitura 
+      e a depuração (debug) enquanto desenvolve.
+    */
+
+    entry: path.resolve(__dirname, "src", "main.js"),
+    /*
+      Aqui diz: "Webpack, comece a ler o projeto a partir deste arquivo".
+      O "path.resolve" garante que ele encontre a pasta "src" e o arquivo "main.js" 
+      a partir da pasta atual do projeto (__dirname).
+     */
+
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    /* 
+      Aqui define o que o Webpack deve "cuspir" no final:
+        "filename": O nome do arquivo final empacotado (main.js).
+        "path": A pasta onde esse arquivo será salvo. No caso, ele criará (ou usará) uma pasta chamada dist (de distribution).
+    */
+}
