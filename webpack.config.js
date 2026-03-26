c2b32ea2b7c0771c8cddc536b6ad9689dae38123
Luiz-Miguel-Pimenta <Miguel876528@gmail.com>
2026-03-26 16:20:46 -0300

    Carreguei o CSS no webpack com o "css-loader" e o "style-loader"

--- webpack.config.js
+++ webpack.config.js
@@ -58,6 +58,31 @@ module.exports = {
   plugins: [
     new HtmlwebpackPlugin({
       template: path.resolve(__dirname, "index.html"),
+      favicon: path.resolve("src", "assets", "scissors.svg"), //Coloca o ícone do projeto no titúlo da página.
     }),
   ],
+
+
+  /*
+    Esse bloco de código é o que "ensina" o Webpack a ler arquivos que não são JavaScript. 
+    Como foi instalado o "style-loader" e o "css-loader", esse código no "webpack.config.js" 
+    é o que coloca esses dois para trabalhar juntos. 
+  */
+  module: {
+    /*
+      Por padrão, o Webpack só entende arquivos .js e .json. O campo module é onde define as regras 
+      de como ele deve tratar outros tipos de arquivos (CSS, imagens, fontes, etc.). 
+    */
+    rules: [ // Cada item dentro de rules é uma instrução para um tipo de arquivo diferente.  
+      {
+        test: /\.css$/, //É uma Expressão Regular (Regex). Serve como um filtro. "Webpack, aplique esta regra em todos os arquivos que terminarem com .css"
+
+        use: ["style-loader", "css-loader"], //Quais ferramentas usar para processar esses arquivos
+        /*
+          css-loader: Ele lê o conteúdo do arquivo CSS e o transforma em uma string dentro do JavaScript. 
+          style-loader: Ele pega essa string e a injeta dentro de uma tag <style> no cabeçalho do site para o estilo aparecer na tela.
+        */
+      },
+    ],
+  },
 }
