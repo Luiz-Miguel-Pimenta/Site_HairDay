ff61edd2fac30eaa6a8a4be6e0f3366ef0a45d16
Luiz-Miguel-Pimenta <Miguel876528@gmail.com>
2026-03-26 22:19:09 -0300

    Adicionei campos de manhã, tarde e noite pelo JS

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -4,6 +4,7 @@ import { openingHours } from "../../utils/opening-hours.js"
 const hours = document.getElementById("hours");
 
 export function hoursLoad({ date }){
+
     const opening = openingHours.map((hour) => {
 
         // Recupera somente a hora. Pega apenas o primeiro valor do array (Ex: "09") e guarda na variável "scheduleHour";
@@ -31,11 +32,23 @@ export function hoursLoad({ date }){
         }
 
         li.textContent = hour;
+
+        if(hour === "9:00" ){
+            hoursHeaderAdd("Manhã");
+        } else if (hour === "13:00") {
+            hoursHeaderAdd("Tarde");
+        } else if (hour === "18:00") {
+            hoursHeaderAdd("Noite");
+        }
+
         hours.append(li);
     })
 }
 
 function hoursHeaderAdd(title) {
     const header = document.createElement("li");
-    header.classList.add("")
+    header.classList.add("hour-period");
+    header.textContent = title;
+
+    hours.append(header);
 }
