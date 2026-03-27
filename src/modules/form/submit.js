39d8d26b6d807ff9a20004de3d44a02ad8562564
Luiz-Miguel-Pimenta <Miguel876528@gmail.com>
2026-03-26 23:09:57 -0300

    Coloquei verificação d entrada no submit.js

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -2,6 +2,7 @@ import dayjs from "dayjs";
 
 const form = document.querySelector(".form");
 const selectedDate = document.getElementById("date");
+const clientName = document.getElementById("client");
 
 // Data atual para formatar o input
 const inputToday = dayjs(new Date()).format("YYYY-MM-DD");
@@ -13,4 +14,41 @@ selectedDate.min = inputToday;
 form.onsubmit = (event) => {
     //Previne o comportamento padrão de carregar a página.
     event.preventDefault();
+
+    try {
+        // Recuperando o nome do cliente.
+        const name = clientName.value.trim();
+
+        if(!name){
+            return alert("Informe o nome do cliente");
+        }
+        
+        // Recupera o horário selecionado.
+        const hourSelected = document.querySelector(".hour-selected");
+
+        // Verifica se tem horário selecionado.
+        if(!hourSelected){
+            return alert("Selecione o horário");
+        }
+
+        // Recupera somente a hora.
+        const [hour] = hourSelected.innerText.split(":");
+
+        // Insere a hora na data.
+        const when = dayjs(selectedDate.value).add(hour, "hour");
+        console.log(when);
+
+        // Gera um ID
+        const id = new Date().getTime();
+
+        console.log({
+            id, 
+            name, 
+            when, 
+        })
+
+    } catch (error) {
+        alert("Não foi possível realizar o agendamento.");
+        console.log(error);
+    }
 }
