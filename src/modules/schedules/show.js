a6424873742657fa4c9ebd7249eba6dbea810f1d
Luiz-Miguel-Pimenta <Miguel876528@gmail.com>
2026-03-28 16:14:51 -0300

    Completei a função de schedulesShow para mostrar os agendamentos

--- src/modules/schedules/show.js
+++ src/modules/schedules/show.js
@@ -17,9 +17,32 @@ export function schedulesShow({ dailySchedules }) {
             const item = document.createElement("li");
             const time = document.createElement("span");
 
-            // Adiciona o id do agendamento.
-            
-        });
+        // Adiciona o id do agendamento.
+        item.setAttribute("data-id", schedule.id);
+
+        // Adiciona o tempo e nome no item.
+        item.innerHTML = `
+            <strong>${dayjs(schedule.when).format("HH:mm")}</strong>
+            <span>${schedule.name}</span>
+            <img
+            src="./src/assets/cancel.svg"
+            alt="Cancelar"
+            class="cancel-icon"
+            />
+        `;
+        
+        // Obtém somente a hora.
+        const hour = dayjs(schedule.when).hour();
+        // Renderiza o agendamento na sessão (manhâ, tarde ou noite).
+        if(hour <= 12){
+            periodMorning.appendChild(item);
+        } else if (hour > 12 && hour <= 18){
+            periodAfternoon.appendChild(item);
+        } else {
+            periodNight.appendChild(item);
+        }
+
+    });
 
     } catch (error) {
         alert("Não foi possível exibir os agendamentos");
